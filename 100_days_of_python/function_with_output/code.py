9d074bd8b0e47a4c41407dc8a63333df74d2f239
joo-interactive-design <86972559+joo-interactive-design@users.noreply.github.com>
2022-02-17 03:24:55 -0500

    Update code.py

--- 100_days_of_python/function_with_output/code.py
+++ 100_days_of_python/function_with_output/code.py
@@ -47,7 +47,43 @@ print(days)
 
 #exercise 3 - calculator : combining dictioinaries and functions
 
+#calculator
 
+#add
+def add(n1, n2):
+  return n1+n2
+
+#subtract
+def subtract(n1, n2):
+  return n1-n2
+
+#multiply
+def multiply(n1, n2):
+  return n1*n2
+
+#divide
+def divide(n1, n2):
+  return n1/n2
+
+#create a dictionary named operations
+operations = {
+"+": add,
+"-": subtract,
+"*": multiply,
+"/": divide
+}
+
+num1 = int(input("What's your first number?: "))
+num2 = int(input("What's your second number?: "))
+#show all symbols in operations
+for symbol in operations:
+  print(symbol)
+operation_symbol = input("pick an operation from above: ")
+
+calculation_function = operations[operation_symbol]
+answer = calculation_function(num1, num2)
+
+print(f"{num1} {operation_symbol} {num2} = {answer}")
 
 
 
