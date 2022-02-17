a213a43e68e202893f61b0d93f846de42da094e2
joo-interactive-design <86972559+joo-interactive-design@users.noreply.github.com>
2022-02-17 00:11:55 -0500

    Update code.py

--- 100_days_of_python/function_with_output/code.py
+++ 100_days_of_python/function_with_output/code.py
@@ -1,1 +1,11 @@
+#exercise 1 - capitalize the first letter of words
+def format_name(f_name, l_name) :
+  first_name = f_name.title()
+  last_name = l_name.title()
+  return f"{first_name} {last_name}"
 
+print(format_name("mina", "kim"))
+#result will be Mina Kim
+
+
+  
