025e7b26062758e56387bef27bd0c13edd9582e0
joo-interactive-design <86972559+joo-interactive-design@users.noreply.github.com>
2022-02-19 02:18:50 -0500

    Update code.py

--- 100_days_of_python/the_blackjack_capstone_project/code.py
+++ 100_days_of_python/the_blackjack_capstone_project/code.py
@@ -1,1 +1,14 @@
 
+#Create a deal_card() function that uses the List below to *return* a random card.
+def deal_card():
+  cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+  card = random.choice(cards)
+  return card
+
+#Deal the user and computer 2 cards each using deal_card() and append().
+user_cards = []
+computer_cards = []
+for _ in range(2):
+  user_cards.append(deal_card())
+  computer_cards.append(deal_card())
+
