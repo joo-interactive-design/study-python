60435c461d28d99cdbd97015dfe502b632b147ed
joo-interactive-design <86972559+joo-interactive-design@users.noreply.github.com>
2022-02-22 23:06:15 -0500

    Update code.py

--- 100_days_of_python/higher_lower_game_project/code.py
+++ 100_days_of_python/higher_lower_game_project/code.py
@@ -1,1 +1,32 @@
 
+#WIP..
+
+import random
+from art import logo
+from game_data import data
+
+score = 0
+# compare_a = data[random_number]
+# against_b = data[random_number]
+random_number = random.randint(1,50)
+data_for_a = data[random_number]
+data_for_b = data[random_number]
+
+
+def answer():
+  if data_for_a['follower_count'] > data_for_b['follower_count']:
+    return 'a'
+  elif data_for_a['follower_count'] < data_for_b['follower_count']:
+    return 'b'
+
+print(answer())
+
+# guess = input("Who has more followers? Type A or B: ")
+
+
+    
+
+# print(f"Compare A: {data_for_a['name']}, {data_for_a['description']}, from {data_for_a['country']}")
+
+
+# print(f"Against B: {data_for_b['name']}, {data_for_b['description']}, from {data_for_b['country']}")
