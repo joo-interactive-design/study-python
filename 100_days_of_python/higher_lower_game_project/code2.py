87d22d94477c38670e548b547ae4c4aeb70cab5f
joo-interactive-design <86972559+joo-interactive-design@users.noreply.github.com>
2022-02-23 23:09:07 -0500

    Create code2.py

---
+++ 100_days_of_python/higher_lower_game_project/code2.py
@@ -0,0 +1,12 @@
+#what i need to do
+#display art
+#generate random 
+#format the account data into printable format
+#ask user for a guess
+#check if user is correct
+##get follower count of each account
+##user if statement to check if user is correct
+#give user the result if it's answer or not, and the score.
+#make the game repeatable
+#making account at position B become the next account at position A
+#clear the screen between rounds
